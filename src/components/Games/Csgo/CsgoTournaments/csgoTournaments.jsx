b3c42386878584a1b82fb184a14c5f21a9ea50bc
-thugnasty- <eterictodd@gmail.com>
2018-08-29 16:09:03 -0600

    Added victor declaration to matches list.

--- src/components/Games/Csgo/CsgoTournaments/csgoTournaments.jsx
+++ src/components/Games/Csgo/CsgoTournaments/csgoTournaments.jsx
@@ -5,12 +5,11 @@ import { Link } from 'react-router-dom';
 
 class CsgoTournaments extends Component {
     render() {
-        console.log("this.props.tournaments:", this.props.tournaments);
         let tournaments = this.props.tournaments;
         let csgoTournamentsList = []
         for (let key in tournaments) {
             if (tournaments.hasOwnProperty(key)) {
-                csgoTournamentsList.push(<Row><Col><h4><Link to={{ pathname: '/games/csgo/tournaments/details/' + JSON.stringify({key}), state: { tournamentName: {key} } }} className="csgoTournamentsList" >{key}</Link></h4></Col></Row>);
+                csgoTournamentsList.push(<Row key={key} ><Col><h4><Link to={{ pathname: '/games/csgo/tournaments/details/' + JSON.stringify({key}), state: { tournamentName: {key} } }} className="csgoTournamentsList" >{key}</Link></h4></Col></Row>);
             }
         }
         return(
--- src/components/Games/Csgo/CsgoTournaments/csgoTournamentsDetails.jsx
+++ src/components/Games/Csgo/CsgoTournaments/csgoTournamentsDetails.jsx
@@ -16,20 +16,33 @@ class CsgoTournamentsDetails extends Component {
                     let team1Id = team1Url.replace( /^\D+/g, '');
                     let team2Url = matches[i].team2_url;
                     let team2Id = team2Url.replace( /^\D+/g, '');
+                    let victorUrl = matches[i].winning_team_url;
                     let team1Name;
                     let team2Name;
+                    let victorName;
                     for (let k = 0; k < teams.length; k++) {
                         if (teams[k].id === parseInt(team1Id, 10)) {
                             team1Name = teams[k].full_name;
+                            if (victorUrl === team1Url) {
+                                victorName = team1Name;
+                            } else {
+                            }
                         } else if (teams[k].id === parseInt(team2Id, 10)) {
                             team2Name = teams[k].full_name;
-                        } else {
+                            if (victorUrl === team2Url) {
+                                victorName = team2Name;
+                            } else {
+                            }
+                        } else  {
+
                         }
                     }
                     csgoMatchesList.push(
                         <Row key={matches[i].id} >
                             <Col>
-                                <h4 className="csgoTournamentMatchesList" >{team1Name} vs. {team2Name}</h4>
+                                <h4>{team1Name} vs. {team2Name}</h4>
+                                <h4>Victor: {victorName}</h4>
+                                <br/>
                             </Col>
                         </Row>
                     );
